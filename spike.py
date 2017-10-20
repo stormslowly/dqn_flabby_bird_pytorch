b4d589e9ed1b5b057539347b5e1130eaca6b5986
stormslowly@gmail.com <stormslowly@gmail.com>
2017-10-20 19:27:16 +0800

    💩

--- spike.py
+++ spike.py
@@ -82,47 +82,61 @@ optimizer = optim.RMSprop(model.parameters())
 def optimize_model(memory):
     if len(memory) < BATCH_SIZE:
         return
+
     transitions = memory.sample(BATCH_SIZE)
 
     batch = Transition(*zip(*transitions))
 
     # Compute a mask of non-final states and concatenate the batch elements
-    non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None,
-                                                batch.next_state)))
 
     # We don't want to backprop through the expected action values and volatile
     # will save us on temporarily changing the model parameters'
     # requires_grad to False!
-    non_final_next_states = Variable(torch.cat([s for s in batch.next_state
-                                                if s is not None]),
-                                     volatile=True)
     state_batch = Variable(torch.cat(batch.state))
+
+    state_batch = torch.stack(map(lambda t: t.state, transitions))
+    action_batch = map(lambda t: t.action, transitions)
+
+    values = model.forward(Variable(state_batch))
+
+    print(action_batch)
+    print('value', values.size())
+    print(values)
+    print(torch.LongTensor([action_batch]).transpose(0, 1))
+    print('===============')
+    print(values.select(1, torch.LongTensor([action_batch]).transpose(0, 1)))
+
+    return
+
     action_batch = Variable(torch.cat(batch.action))
+
     reward_batch = Variable(torch.cat(batch.reward))
 
     # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
     # columns of actions taken
     state_action_values = model(state_batch).gather(1, action_batch)
 
+    # print(state_action_values)
+
     # Compute V(s_{t+1}) for all next states.
-    next_state_values = Variable(torch.zeros(BATCH_SIZE).type(Tensor))
-    next_state_values[non_final_mask] = model(non_final_next_states).max(1)[0]
-    # Now, we don't want to mess up the loss with a volatile flag, so let's
-    # clear it. After this, we'll just end up with a Variable that has
-    # requires_grad=False
-    next_state_values.volatile = False
-    # Compute the expected Q values
+    # next_state_values = Variable(torch.zeros(BATCH_SIZE).type(Tensor))
+    # next_state_values[non_final_mask] = model(non_final_next_states).max(1)[0]
+    # # Now, we don't want to mess up the loss with a volatile flag, so let's
+    # # clear it. After this, we'll just end up with a Variable that has
+    # # requires_grad=False
+    # next_state_values.volatile = False
+    # # Compute the expected Q values
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-    # Compute Huber loss
-    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
-
-    # Optimize the model
-    optimizer.zero_grad()
-    loss.backward()
-    for param in model.parameters():
-        param.grad.data.clamp_(-1, 1)
-    optimizer.step()
+    #
+    # # Compute Huber loss
+    # loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
+    #
+    # # Optimize the model
+    # optimizer.zero_grad()
+    # loss.backward()
+    # for param in model.parameters():
+    #     param.grad.data.clamp_(-1, 1)
+    # optimizer.step()
 
 
 for _ in range(1):
@@ -135,8 +149,8 @@ for _ in range(1):
         done = env.step(action)
         img.set_data(tensor_image_to_numpy_image(env.current_state))
         plt.draw()
-        plt.pause(0.1)
         if done:
             break
 
-    print(len(env.mem))
+    BATCH_SIZE = 3
+    optimize_model(env.mem)
